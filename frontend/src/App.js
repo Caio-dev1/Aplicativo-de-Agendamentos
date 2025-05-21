32fdd71faeef4076eaac0d3c0c2a13bd673fd515
Caio-dev1 <caiocesar.developer1@gmail.com>
2025-05-21 20:04:27 -0300

    adicionando compatibilidade com vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { useEffect, useState } from 'react';
 import React from 'react';
 import Agendamentos from './componentes/Agendamentos/agendamento';
 
+const API_BASE = process.env.REACT_APP_API_BASE_URL;
+
 function App() {
   const [agendamentos, setAgendamentos] = useState([])
 
 
   useEffect(() => {
-    fetch("/api/agendamentos")
+    fetch(`${API_BASE}/agendamentos`)
     .then((res) => res.json())
     .then((data) => setAgendamentos(data))
     .catch((err) => alert("Alguma coisa deu errado ao tenta acessar o banco de dados", err))
