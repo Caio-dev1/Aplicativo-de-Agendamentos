4ef6d2d37b97fe7df24409299ba37d0c2a85e084
Caio-dev1 <caiocesar.developer1@gmail.com>
2025-05-20 22:21:18 -0300

    .

--- api/server.js
+++ api/server.js
@@ -1,19 +1,13 @@
-
 const jsonServer = require('json-server');
 const path = require('path');
 
-
-const app = jsonServer.create();
-
+const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
-
 const middlewares = jsonServer.defaults();
 
-app.use(middlewares);
-app.use(router);
+server.use(middlewares);
+server.use(router);
 
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`JSON Server rodando em http://localhost:${PORT}`);
-});
+module.exports = (req, res) => {
+  server(req, res);
+};
