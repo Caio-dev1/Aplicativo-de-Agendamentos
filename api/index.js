298caf33b3dfbfa2ddefc2345383d077eb3138c0
Caio-dev1 <caiocesar.developer1@gmail.com>
2025-05-21 19:45:56 -0300

    .

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
 const middlewares = jsonServer.defaults();
+
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-});
+
+
+module.exports = server;
