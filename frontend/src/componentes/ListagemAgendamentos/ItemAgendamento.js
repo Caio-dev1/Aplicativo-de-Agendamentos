c66bca8982ab88d96caa1db8b36a98b09c9deaf3
Caio-dev1 <caiocesar.developer1@gmail.com>
2025-05-29 21:36:05 -0300

    Adicionando componente completo

--- frontend/src/componentes/ListagemAgendamentos/ItemAgendamento.js
+++ frontend/src/componentes/ListagemAgendamentos/ItemAgendamento.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Relogio from "../../img/Relogio.png"
+import Calendario from "../../img/CalendarioCinza.png"
 
 const AgendamentoItem = styled.div`
  background-color: #FFFFFF;
@@ -8,32 +10,117 @@ const AgendamentoItem = styled.div`
     height: 96px;
     border-radius: 16px;
 `
+const Seta = styled.img`
+   height: 12px;
+`
+const SetaContainer = styled.div`
+  padding-right: 2rem;
+  display: flex;
+  align-items: center;
+  height: 100%;
+`;
+
+const Icone = styled.img`
+ heigth: 12px;
+`
+const ItemTexto = styled.p`
+ color: #A098AE;
+ font-family: Poppins;
+font-weight: 400;
+font-size: 14px;
+line-height: 100%;
+letter-spacing: 0%;
+margin: 0;
+`
+const ItemTitulo = styled.h3`
+ font-family: Poppins;
+font-weight: 600;
+font-size: 18px;
+line-height: 100%;
+letter-spacing: 0%;
+color: #374557;
+white-space: nowrap;
+margin: 0;
+`
+const Conteudo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 12px 16px;
+  flex: 1;
+  gap: 1rem;
+   white-space: nowrap;
+`;
+const LinhaSuperior = styled.div`
+  display: flex;
+  gap: 3rem;
+  align-items: center;
+`;
+const ItemDot = styled.div`
+width: 24px;
+height: 24px;
+border-radius: 8px;
+background-color: #DBDBDB;
+`
+const LinhaInferior = styled.div`
+ display: flex;
+ align-items: center;
+ gap: 8px;
+`
+const DataEHora = styled.div`
+ display: flex;
+ gap: 30px;
+ align-items: center;
+`;
+
+const DataOuHora = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+function ItemAgendamento({ item, corBase, seta }) {
 
 const BarraLateral = styled.div`
   width: 16px;
   height: 96px;
-  background-color: #4CBC9A;
+  background-color: ${props => props.corBase};
   border-top-left-radius: 20px;
   border-bottom-left-radius: 20px;
 `;
 
-const Seta = styled.img`
- heigth: 12px;
-`
-
-function ItemAgendamento({ item }) {
-    return ( 
-        
-        <AgendamentoItem>
-            <BarraLateral></BarraLateral>
-            {item.Nome} , 
-            {item.Data} ,
-            {item.Hora} ,
-            {item.Descrição} ,
-            {item.Status}
-            <Seta src={""} alt="Seta Apontando Para direita"/>
-        </AgendamentoItem>
-    );
+function formatarDataBr(dataISO) {
+  const data = new Date(dataISO);
+  return data.toLocaleDateString("pt-BR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 }
-
+  return (
+    <AgendamentoItem>
+      <BarraLateral corBase={corBase} />
+      <Conteudo>
+        <LinhaSuperior>
+          <ItemTitulo>{item.Nome}</ItemTitulo>
+            <DataEHora>
+            <DataOuHora>
+                <Icone src={Calendario} />
+                <ItemTexto>{formatarDataBr(item.Data)}</ItemTexto>
+            </DataOuHora>
+            <DataOuHora>
+                <Icone src={Relogio} />
+                <ItemTexto>{item.Hora}H</ItemTexto>
+            </DataOuHora>
+            </DataEHora>
+        </LinhaSuperior>
+        <LinhaInferior>
+        <ItemDot></ItemDot>
+        <ItemTexto>{item.Descrição}</ItemTexto>
+        </LinhaInferior>
+      </Conteudo>
+    <SetaContainer>
+    <Seta src={seta} alt="Seta Apontando Para direita" />
+    </SetaContainer>
+    </AgendamentoItem>
+  );
+}
 export default ItemAgendamento;
--- frontend/src/componentes/ListagemAgendamentos/ListaAgendamento.js
+++ frontend/src/componentes/ListagemAgendamentos/ListaAgendamento.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import ItemAgendamento from "./ItemAgendamento";
 import ListaAgendamentoTitulo from "./ListaAgendamentoTitulo";
+import SetaConcluido from "../../img/SetaConcluido.png";
+import SetaAndamento from "../../img/SetaAndamento.png";
+import SetaExpirado from "../../img/SetaExpirado.png";
 
 const Container = styled.div`
   display: flex;
@@ -26,6 +29,32 @@ const AgendamentoLi = styled.li`
 function ListaAgendamentos({ agendamentos }) {
   const agendamentosRenderizados = agendamentos.filter(a => a.id <= 5);
 
+function handleStatusAgendamento(status) {
+  switch (status) {
+    case "C":
+      return "#4CBC9A";
+    case "A":
+      return "#FEC64F";
+    case "E":
+      return "#FC6B57";
+    default:
+      return "#CCCCCC";
+  }
+}
+function handleStatusSeta(status) {
+  switch (status) {
+    case "C":
+      return SetaConcluido;
+    case "A":
+      return SetaAndamento;
+    case "E":
+      return SetaExpirado;
+    default:
+      return ""; 
+  }
+}
+
+
   return (
   <>
     <Container>
@@ -33,7 +62,7 @@ function ListaAgendamentos({ agendamentos }) {
       <AgendamentosLista>
         {agendamentosRenderizados.map(item => (
           <AgendamentoLi key={item.id}>
-            <ItemAgendamento item={item} />
+            <ItemAgendamento item={item} seta={handleStatusSeta(item.Status)} corBase={handleStatusAgendamento(item.Status)}/>
           </AgendamentoLi>
         ))}
       </AgendamentosLista>
