544a651143f3e25ef9240fa61059d8963ccbfa59
Caio-dev1 <caiocesar.developer1@gmail.com>
2025-07-01 20:11:57 -0300

    Foto de Perfil

--- frontend/src/componentes/PixeonTopBar/BarraTopo.js
+++ frontend/src/componentes/PixeonTopBar/BarraTopo.js
@@ -1,16 +1,43 @@
 import AgendamentoTitulo from "./AgendamentoTitulo";
 import styled from "styled-components";
+import UsuarioPerfil from "../../img/UsuarioPerfil.png";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const TopoBarraSecao = styled.section`
   background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 16px;
+`;
+
+const PerfilWrapper = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 5px solid #4CBC9A;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+`;
+const PerfilImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  cursor: pointer;
 `;
 
 function Barratopo() {
+  const navegar = useNavigate()
   return (
     <TopoBarraSecao>
       <AgendamentoTitulo />
+      <PerfilWrapper>
+        <PerfilImage onClick={() => navegar("/login", { replace: true })} src={UsuarioPerfil} alt="Foto de Perfil de Usuário" />
+      </PerfilWrapper>
     </TopoBarraSecao>
   );
 }
 
 export default Barratopo;
+
