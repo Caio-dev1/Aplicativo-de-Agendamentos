9d5e16da60c3ca792f10ebccb75b817cfb82e396
Caio-dev1 <caiocesar.developer1@gmail.com>
2025-07-04 20:53:18 -0300

    .

--- api/index.js
+++ api/index.js
@@ -1,15 +1,10 @@
-const path = require('path');
-const jsonServer = require('json-server');
-const fs = require('fs');
+const jsonServer = require("json-server");
 
-const localPath   = path.join(__dirname, 'db.json');       
-const lambdaPath  = path.join(__dirname, 'api', 'db.json');
-const dbPath      = fs.existsSync(localPath) ? localPath : lambdaPath;
+const server = jsonServer.create();
 
-const server      = jsonServer.create();
-const router      = jsonServer.router(dbPath);
+const router = jsonServer.router("api/db.json");
+
 const middlewares = jsonServer.defaults();
-
 server.use(middlewares);
 server.use(router);
 
